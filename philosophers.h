a36721b14a4511bf31b2aa4c1c81ebd115d1685d
spydersy <acbe1325@aol.com>
2021-07-20 08:00:45 -0400

    ADD Makefile *.c *.h

---
+++ philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelarif <abelarif@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/20 07:55:49 by abelarif          #+#    #+#             */
+/*   Updated: 2021/07/20 07:58:12 by abelarif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+#define PHILOSOPHERS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#endif
