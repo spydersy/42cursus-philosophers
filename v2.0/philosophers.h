04806d15c0dee3cb1fc0ab0154e4f0daff116531
spydersy <acbe1325@aol.com>
2021-08-14 21:48:54 -0400

    last edit : take forks

--- v2.0/philosophers.h
+++ v2.0/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: abelarif <abelarif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/14 12:35:08 by abelarif          #+#    #+#             */
-/*   Updated: 2021/08/14 20:14:29 by abelarif         ###   ########.fr       */
+/*   Updated: 2021/08/14 21:28:26 by abelarif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,32 @@
 
 typedef struct  s_philosophers
 {
+    int     id;
+    int     number_of_meal;
+    int     time_to_eat;
+    int     time_to_sleep;
+
+    pthread_mutex_t     *status_mutex;
+    pthread_mutex_t     *left_mutex;
+    pthread_mutex_t     *right_mutex;
 
 }               t_philosophers;
 
 
 typedef struct s_data
 {
+    int number_of_philosophers;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int time_to_think;
+    int repeat_eat;
 
+    pthread_mutex_t *forks_mutex;
+    pthread_mutex_t status_mutex;
+    pthread_mutex_t status;
+
+    t_philosophers  *philosophers;
 }	t_data;
 
 //utils.c
